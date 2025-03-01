51a99bf029bcc3a29fe21f10018fc9410cd5429d
NPotokin <npotokin@gmail.com>
2025-03-01 21:29:22 +0300

    Fix: fixed assets root and categories load bug

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,52 +1,19 @@
 import { Button } from '@/components/ui/button';
 import Title from '@/components/ui/title';
-import { useAppDispatch } from '@/hooks/useAppDispatch';
-import { useTypedSelector } from '@/hooks/useTypedSelector';
-import { fetchCategoriesThisMonth } from '@/store/categories/categories.Thunk';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CategoryCard } from './CategoryCard';
-import { AccountCardSkeleton } from '../Accounts/AccountCardSkeleton';
 
 const Categories: React.FC = () => {
 	const navigate = useNavigate();
-	const dispatch = useAppDispatch();
-	const { categories, loading, error } = useTypedSelector((state) => state.categories);
-
-	// Fetch categories only if not already loaded or if the list is empty
-	useEffect(() => {
-		if (!loading && !error && categories.length === 0) {
-			dispatch(fetchCategoriesThisMonth());
-		}
-	}, [dispatch, categories.length, loading, error]);
 
 	return (
 		<div className="flex flex-col">
 			<Title name="Categories" />
 
-			{/* Loading State */}
-			{loading ? (
-				<>
-					<AccountCardSkeleton />
-					<AccountCardSkeleton />
-					<AccountCardSkeleton />
-				</>
-			) : error ? (
-				<p className="text-red-500 text-center py-4">
-					Failed to load categories.
-					<Button variant="link" onClick={() => dispatch(fetchCategoriesThisMonth())}>
-						Retry
-					</Button>
-				</p>
-			) : categories.length === 0 ? (
-				<p className="text-gray-500 text-center py-4">No categories found.</p>
-			) : (
-				<div className="overflow-auto space-y-1 h-[64vh]">
-					{categories.map((category) => (
-						<CategoryCard key={category.id} category={category} />
-					))}
-				</div>
-			)}
+			<div className="overflow-auto space-y-1 h-[64vh]">
+					<CategoryCard/>
+			</div>
 
 			<Button type="button" variant="default" className="mx-4 p-6" onClick={() => navigate('/categories/add')}>
 				Add Category
--- src/pages/Categories/CategoryCard.tsx
+++ src/pages/Categories/CategoryCard.tsx
@@ -1,43 +1,74 @@
 import { Card, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
-import { Category } from '@/store/categories/catgoriesSlice';
+import { useAppDispatch } from '@/hooks/useAppDispatch';
+import { useTypedSelector } from '@/hooks/useTypedSelector';
+import { fetchCategoriesThisMonth } from '@/store/categories/categories.Thunk';
+import { useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AccountCardSkeleton } from '../Accounts/AccountCardSkeleton';
 
-interface CategoryProps {
-	category: Category;
-}
-
-export const CategoryCard: React.FC<CategoryProps> = ({ category }) => {
+export const CategoryCard: React.FC = () => {
 	const navigate = useNavigate();
+	const dispatch = useAppDispatch();
+	const { categories, loading, error } = useTypedSelector((state) => state.categories);
 
-	return (
-		<Card className="mx-4" onClick={() => navigate(`/categories/${category.id}`)}>
-			<CardHeader>
-				<div className="flex flex-col">
-					<CardTitle
-						className={`text-2xl font-light mb-2 flex justify-between ${category.type === 'spending' ? 'text-red-800' : 'text-green-800'}`}
-					>
-						<p>{category.name}</p>
-					</CardTitle>
-					<CardDescription>
-						<div className="flex justify-between">
-							<p className="text-primary pr-2 w-1/3 text-xs">
-								{category.type === 'spending' ? 'Budgeted:' : 'Expected:'}
-							</p>
-							<p className="text-primary pr-2 w-1/3 text-xs">
-								{category.type === 'spending' ? 'Used:' : 'Recieved:'}
-							</p>
-							<p className="text-primary pr-2 w-1/3 text-xs">Left:</p>
-						</div>
-						<div className="flex justify-between">
-							<p className=" text-xl pr-2 w-1/3 font-mono">{category.category_limit}</p>
-							<p className="text-red-800 text-xl  pr-2 w-1/3 font-mono">{category.used}</p>
-							<p className="text-green-800 text-xl  pr-2 w-1/3 font-mono">
-								{category.category_limit! - category.used}
-							</p>
-						</div>
-					</CardDescription>
-				</div>
-			</CardHeader>
-		</Card>
-	);
+	useEffect(() => {
+		dispatch(fetchCategoriesThisMonth());
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	const categoriesCards = useMemo(() => {
+		return categories.map((category) => (
+			<Card className="mx-4" onClick={() => navigate(`/categories/${category.id}`)}>
+				<CardHeader>
+					<div className="flex flex-col">
+						<CardTitle
+							className={`text-2xl font-light mb-2 flex justify-between ${
+								category.type === 'spending' ? 'text-red-800' : 'text-green-800'
+							}`}
+						>
+							<p>{category.name}</p>
+						</CardTitle>
+						<CardDescription>
+							<div className="flex justify-between">
+								<p className="text-primary pr-2 w-1/3 text-xs">
+									{category.type === 'spending' ? 'Budgeted:' : 'Expected:'}
+								</p>
+								<p className="text-primary pr-2 w-1/3 text-xs">
+									{category.type === 'spending' ? 'Used:' : 'Recieved:'}
+								</p>
+								<p className="text-primary pr-2 w-1/3 text-xs">Left:</p>
+							</div>
+							<div className="flex justify-between">
+								<p className=" text-xl pr-2 w-1/3 font-mono">{category.category_limit}</p>
+								<p className="text-red-800 text-xl  pr-2 w-1/3 font-mono">{category.used}</p>
+								<p className="text-green-800 text-xl  pr-2 w-1/3 font-mono">
+									{category.category_limit! - category.used}
+								</p>
+							</div>
+						</CardDescription>
+					</div>
+				</CardHeader>
+			</Card>
+		));
+	}, [categories, navigate]);
+
+	if (loading && categories.length === 0) {
+			return (
+				<>
+					<AccountCardSkeleton />
+					<AccountCardSkeleton />
+					<AccountCardSkeleton />
+				</>
+			);
+		}
+	
+		if (error) {
+			return <p className="text-red-500 text-center py-4">Failed to load categories.</p>;
+		}
+	
+		if (categories.length === 0) {
+			return <p className="text-primary text-lg text-center py-6">No categories available</p>;
+		}
+	
+		return <>{categoriesCards}</>;
 };
